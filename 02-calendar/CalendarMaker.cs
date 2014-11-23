ad16474c5afbb361af18db5f0b211b1a42186eba
fokychuk_book <fokychuk47@ya.ru>
2014-11-23 20:13:03 +0500

    init

---
+++ 02-calendar/CalendarMaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_calendar
+{
+    public class CalendarMaker
+    {
+        DateTime date;
+        public CalendarMaker(DateTime date)
+        {
+            this.date = date;
+        }
+    }
+}
